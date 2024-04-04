0849f64118025fdae57665d803caf1acaf2af572
yiwei <rot1998@163.com>
2024-04-05 01:26:24 +0800

    function v1正确

---
+++ function_v1.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.optimize import minimize
+
+def cal_v(rn,rg,x):
+    '''
+    函数体内写计算v的表达式
+    :param rn: 待优化系数
+    :param rg: 待优化系数
+    :param x: x样本
+    :return: v
+    '''
+    return rn+rg*x
+def overlay_funcs(x_arr): #合成函数
+    return lambda rn,rg:[(lambda rn,rg: cal_v(rn,rg,x))(rn,rg) for x in x_arr]
+def loss_func(x_arr,y): #合成函数和样本y的差距损失
+   return lambda args : sum((np.array(overlay_funcs(x_arr)(args[0],args[1]))-y)**2)
+if __name__ == '__main__':
+    # print(overlay_funcs([1,1])(1,2))
+    # print(loss_func([1,2,3,4],[0,0,0,1])([1,2]))
+    args=np.array([1,2]) #rn,rg 初始值
+    x_arr = [1,2,3,4]
+    y=np.array([5,6,7,8],dtype=np.float32)
+    res = minimize(loss_func(x_arr,y), args, method='SLSQP')
+    print(res.success) #优化是否成功
+    print(res.x) #最后得到的rn, rg 值
+    print(res.fun) #最终损失函数的值
--- functions.py
+++ functions.py
@@ -1,7 +1,12 @@
 import copy
 import random
+import typing
+
+import numpy as np
+
+
 def cal_v(rn,rg,x):
-    return rn*x+rg
+    return rn*x+2*rg
 def gen_args(n):
     args=[]
     for i in range(n):
@@ -20,4 +25,10 @@ def overlay_funcs(args):
         rn,rg = args[i:i+2]
         # v = lambda x: cal_v(rn,rg,x)
         params.append((rn,rg))
-    return  lambda x: [(lambda x: cal_v(rn,rg,x))(x) for rn,rg in params]
+
+    return lambda x:sum([(lambda x: cal_v(rn,rg,x))(x) for rn,rg in params])
+def loss_func(args,x,y):
+    vf=np.vectorize(overlay_funcs(args))
+    y_comp=vf(x)
+    return ((y_comp-y)**2).sum()
+
