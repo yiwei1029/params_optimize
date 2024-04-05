ff6e43792bde60b11f71fed306c6baf24cd3fb78
yiwei <rot1998@163.com>
2024-04-05 11:41:28 +0800

    debug add 'v2'

--- function_v2.py
+++ function_v2.py
@@ -10,14 +10,17 @@ def cal_v(rn,rg,x):
     :return: v
     '''
     return rn+rg*x
-def overlay_funcs(x_arr): #合成函数
+def overlay_funcs(x_arr): #映射x序列到v
     return lambda rn,rg:[(lambda rn,rg: cal_v(rn,rg,x))(rn,rg) for x in x_arr]
 def loss_func(x_arr,y): #合成函数和样本y的差距损失
-   return lambda args : sum((np.array(overlay_funcs(x_arr)(args[0],args[1]))-y)**2)
+
+   return lambda args : sum(((np.array([overlay_funcs(x_arr)(args[i],args[i+1])
+                                        for i in range(0,len(args),2)])
+                         .sum(0))-y)**2)
 if __name__ == '__main__':
-    print(overlay_funcs([1,1])(1,2))
-    # print(loss_func([1,2,3,4],[0,0,0,1])([1,2]))
-    args=np.array([1,2]) #rn,rg 初始值
+    # print(overlay_funcs([1,1])(1,2))
+    # print(loss_func([1,2,3,4],[0,0,0,1])([1,2,3,4]))
+    args=np.array([1,2,3,4,5,6]) #rn,rg 初始值
     x_arr = [1,2,3,4]
     y=np.array([5,6,7,8],dtype=np.float32)
     res = minimize(loss_func(x_arr,y), args, method='SLSQP')
